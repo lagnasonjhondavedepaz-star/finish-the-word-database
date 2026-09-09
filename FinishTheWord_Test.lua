e84b0374f09f869272af79813e5a5e86e77da566
lagnasonjhondavedepaz-star <lagnason.jhondave.depaz@gmail.com>
2026-09-10 02:17:16 +0800

    add more top

--- FinishTheWord_Test.lua
+++ FinishTheWord_Test.lua
@@ -32,7 +32,7 @@ end)
 -- TOP BUTTONS CONTAINER (Keeps them centered side-by-side)
 local topButtonsContainer = Instance.new("Frame")
 topButtonsContainer.Size = UDim2.new(0, 0, 0, 28)
-topButtonsContainer.Position = UDim2.new(0.5, 0, 0, -5)
+topButtonsContainer.Position = UDim2.new(0.5, 0, 0, -15)
 topButtonsContainer.AnchorPoint = Vector2.new(0.5, 0)
 topButtonsContainer.BackgroundTransparency = 1
 topButtonsContainer.AutomaticSize = Enum.AutomaticSize.X
